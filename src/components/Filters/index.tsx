abf9f7429520c6fa44849b230a85a345ed6d3a15
Sonali0501 <nogjasonali8@gmail.com>
2024-05-04 09:36:15 +0530

    fix: filters responsive

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -8,10 +8,14 @@ const experienceOptions = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
 const Filters: React.FC = () => {
     return (
         <div className='filters-container'>
+            <div className='filter-unit first'>
             <SelectInput label='Experience' name='experience' options={experienceOptions} />
             <SelectInput label='Role' name='role' options={roleOptions} />
+            </div>
+            <div className='filter-unit'>
             <TextInput placholder='Location' name='location' />
             <TextInput placholder='Company' name='company' />
+            </div>
         </div>
     )
 }
